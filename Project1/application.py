ce52bcf773f1e6b16c8229a09b4d3b6c78447e67
Naveen Sai Ram <naveenkatragadda623@gmail.com>
2020-04-23 22:27:48 +0530

    Authenticate Task1

---
+++ Project1/application.py
@@ -0,0 +1,104 @@
+import os
+
+from flask import Flask, session,url_for,redirect
+from flask import Flask,render_template,request,flash
+from flask_session import Session
+from  datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from model import *
+
+app = Flask(__name__)
+app.secret_key = "secret"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+engine = create_engine(os.getenv("DATABASE_URL"))
+Session(app)
+db.init_app(app)
+
+with app.app_context():
+	db.create_all()
+
+@app.route('/')
+def indexed():
+    if 'username' in session:
+        username = session['username']
+        return 'Logged in as ' + username + '<br>' + \
+         "<b><a href = '/logout'>click here to log out</a></b>"
+    return "You are not logged in <br><a href = '/login'></b>" + \
+      "click here to log in</b></a>"
+  
+
+@app.route("/register")
+def index():    
+    return render_template("Registration.html")
+
+@app.route("/User",methods = ["GET","POST"])
+def User():
+    
+    db.create_all()
+    if request.method == "POST":
+        name = request.form.get("fname")
+        email = request.form.get("Email")
+        pswd  = request.form.get("password")
+        register = Registration(Email = email, Password = pswd, datetime = str(datetime.now()))
+    
+        
+        db.session.add(register)
+        db.session.commit()
+        return render_template("User.html")
+            
+        # except Exception :
+        #     # return render_template("error.html", errors = "Details are already given")
+    return render_template("error.html", errors = "Details are already given")
+    
+@app.route("/admin")
+
+def Member():
+      """List all users."""
+     
+      Member = Registration.query.all()
+      return render_template("show.html", Member = Member)
+
+@app.route("/auth",methods = ["GET","POST"])
+def authenticate():
+    Registration.query.all()
+    name = request.form.get("fname")
+    email = request.form.get("Email")
+    pswd  = request.form.get("password")
+       
+    try:
+        Member = db.session.query(Registration).filter(Registration.Email == email).all()
+        if len(Member) >0 :
+            
+            print(len(Member))
+            print(Member[0].Password)
+            if Member[0].Email == email and Member[0].Password == pswd:
+                print(Member[0].Firstname)
+                session['username'] = request.form.get("Email")
+                return redirect(url_for('indexed'))   
+            else:
+                return render_template("error.html", errors = " Username / Password is incorrect")
+        else:
+            return "<h1> Please Login / Register </h1>"
+        
+                    
+        
+    except Exception :
+	    return render_template("error.html", errors = "Details are already given")              
+        
+   
+        
+    
+
+
+    
+
+  
+
+
+
+   
+
+
+            
